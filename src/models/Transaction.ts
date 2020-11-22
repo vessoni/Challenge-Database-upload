4511db8a59fc9a1b54388d7595b56fbbbd79cf66
Alexandre <av.vessoni@gmail.com>
2020-11-22 20:53:11 +0000

    Create Migrations + Models - Categories / Transactions

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,41 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+import Category from './Category';
+
+export enum Type {
+  income = 'income',
+  outcome = 'outcome',
+}
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
-  type: 'income' | 'outcome';
+  @Column({ type: 'enum', enum: Type })
+  type: Type;
 
+  @Column()
   value: number;
 
+  @OneToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
