e80a4b627196c6e08783a4717c58e70c289fdd21
Alexandre <av.vessoni@gmail.com>
2020-12-02 17:58:06 +0000

    Added Cors

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import 'reflect-metadata';
 import 'dotenv/config';
 
@@ -13,6 +14,7 @@ createConnection();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
